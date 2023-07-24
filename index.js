1b4a4b02dae5b72ecea5aff1110481b6d8ecabbd
Basharirani23 <super_boshbosh@hotmail.com>
2023-07-24 22:59:16 +0300

    lambda

---
+++ index.js
@@ -0,0 +1,57 @@
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
+exports.handler = async (event, context) => {
+    const bucket = event.Records[0].s3.bucket.name;
+    const key = event.Records[0].s3.object.key;
+
+    try {
+        const imagesJsonData = await getImagesJsonData(bucket);
+
+        const imageMetadata = {
+            name: key,
+            size: event.Records[0].s3.object.size,
+            type: event.Records[0].s3.object.contentType,
+            // Add more metadata fields as needed
+        };
+
+        const existingImageIndex = imagesJsonData.findIndex(
+            (image) => image.name === key
+        );
+        if (existingImageIndex !== -1) {
+            imagesJsonData[existingImageIndex] = imageMetadata;
+        } else {
+            imagesJsonData.push(imageMetadata);
+        }
+
+        await uploadImagesJson(bucket, imagesJsonData);
+
+        console.log("image.json updated successfully.");
+    } catch (error) {
+        console.log("Error updating image.json:", error);
+    }
+};
+
+async function getImagesJsonData(bucket) {
+    try {
+        const response = await s3
+            .getObject({ Bucket: bucket, Key: "image.json" })
+            .promise();
+        return JSON.parse(response.Body.toString());
+    } catch (error) {
+        if (error.code === "NoSuchKey") {
+            return [];
+        }
+        throw error;
+    }
+}
+
+async function uploadImagesJson(bucket, data) {
+    await s3
+        .putObject({
+            Bucket: bucket,
+            Key: "image.json",
+            Body: JSON.stringify(data),
+        })
+        .promise();
+}
